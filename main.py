03ea939bfe923f23107ea7c50b280452cdafb70f
Sedelmaier Stefan <stefan@sedelmaier.at>
2023-04-10 17:55:28 +0200

    extract Explosion to its own file

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pygame import mixer
 import button
 from engine.animations import Action
 from engine.animations import ActionAnimation
+from engine.explosion import Explosion
 from engine.screenfade import ScreenFade
 
 mixer.init()
@@ -562,8 +563,7 @@ class Grenade(pygame.sprite.Sprite):
         if self.timer <= 0:
             self.kill()
             grenade_fx.play()
-            explosion = Explosion(self.rect.x, self.rect.y, 0.5)
-            explosion_group.add(explosion)
+            explosion_group.add(Explosion(self.rect.x, self.rect.y, 0.5))
             # do damage to anyone that is nearby
             if abs(self.rect.centerx - world.player.rect.centerx) < TILE_SIZE * 2 and \
                     abs(self.rect.centery - world.player.rect.centery) < TILE_SIZE * 2:
@@ -572,38 +572,6 @@ class Grenade(pygame.sprite.Sprite):
                 if abs(self.rect.centerx - enemy.rect.centerx) < TILE_SIZE * 2 and \
                         abs(self.rect.centery - enemy.rect.centery) < TILE_SIZE * 2:
                     enemy.health -= 50
-
-
-class Explosion(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
-        pygame.sprite.Sprite.__init__(self)
-        self.images = []
-        for num in range(1, 6):
-            img = pygame.image.load(f'img/explosion/exp{num}.png').convert_alpha()
-            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-            self.images.append(img)
-        self.frame_index = 0
-        self.image = self.images[self.frame_index]
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-        self.counter = 0
-
-    def update(self):
-        # scroll
-        self.rect.x += screen_scroll
-
-        EXPLOSION_SPEED = 4
-        # update explosion amimation
-        self.counter += 1
-
-        if self.counter >= EXPLOSION_SPEED:
-            self.counter = 0
-            self.frame_index += 1
-            # if the animation is complete then delete the explosion
-            if self.frame_index >= len(self.images):
-                self.kill()
-            else:
-                self.image = self.images[self.frame_index]
 
 
 # create screen fades
@@ -667,11 +635,12 @@ while run:
         # update and draw groups
         bullet_group.update(screen)
         grenade_group.update()
-        explosion_group.update()
+        explosion_group.update(screen_scroll)
         item_box_group.update()
         decoration_group.update()
         water_group.update()
         exit_group.update()
+
         bullet_group.draw(screen)
         grenade_group.draw(screen)
         explosion_group.draw(screen)
