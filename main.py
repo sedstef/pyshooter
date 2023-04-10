94f2588e88f758c2fd125699283d222fe87427c3
Sedelmaier Stefan <stefan@sedelmaier.at>
2023-04-10 11:45:31 +0200

    jump a bit higher

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ class Soldier(pygame.sprite.Sprite):
 
 		#jump
 		if self.jump == True and self.in_air == False:
-			self.vel_y = -11
+			self.vel_y = -12
 			self.jump = False
 			self.in_air = True
 
