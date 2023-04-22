670defc6dba05aa583a011012751265be04614db
Sedelmaier Stefan <stefan@sedelmaier.at>
2023-04-22 10:14:16 +0200

    extract color constants to colors.py

--- pyshooter/main.py
+++ pyshooter/main.py
@@ -3,14 +3,11 @@ from enum import StrEnum
 
 import pygame
 from pygame import mixer, Surface, Rect
-from pygame.locals import (
-    KEYDOWN,
-    K_ESCAPE,
-    QUIT
-)
+from pygame.locals import KEYDOWN, K_ESCAPE, QUIT
 from pygame.sprite import Sprite, Group
 
 from pyshooter import resources, keyboard
+from pyshooter.colors import BG, RED, WHITE, GREEN, BLACK, PINK
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
@@ -26,14 +23,6 @@ COLS = 150
 TILE_SIZE = SCREEN_HEIGHT // ROWS
 TILE_TYPES = 21
 MAX_LEVELS = 3
-
-# define colours
-BG = (144, 201, 120)
-RED = (255, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-PINK = (235, 65, 54)
 
 
 class Background:
